412cbbd9781d980a02fa0b1e5870a4ccbb9b5f0f
pupu <pudiva@github.com>
2022-10-28 15:28:11 +0000

    WIP: split reload() into reloadSchema() and reloadStats()
    Signed-off-by: pupu <pudiva@github.com>

--- go/vt/vttablet/tabletserver/schema/engine.go
+++ go/vt/vttablet/tabletserver/schema/engine.go
@@ -311,8 +311,14 @@ func (se *Engine) ReloadAt(ctx context.Context, pos mysql.Position) error {
 	return nil
 }
 
-// reload reloads the schema. It can also be used to initialize it.
+// reload reloads the schema and statistics. It can also be used to initialize it.
 func (se *Engine) reload(ctx context.Context) error {
+	reloadSchema(ctx)
+	reloadStats(ctx)
+}
+
+// reloadSchema reloads the schema. It can also be used to initialize it.
+func (se *Engine) reloadSchema(ctx context.Context) error {
 	defer func() {
 		se.env.LogError()
 	}()
@@ -353,12 +359,6 @@ func (se *Engine) reload(ctx context.Context) error {
 		tableName := row[0].ToString()
 		curTables[tableName] = true
 		createTime, _ := evalengine.ToInt64(row[2])
-		fileSize, _ := evalengine.ToUint64(row[4])
-		allocatedSize, _ := evalengine.ToUint64(row[5])
-
-		// publish the size metrics
-		se.tableFileSizeGauge.Set(tableName, int64(fileSize))
-		se.tableAllocatedSizeGauge.Set(tableName, int64(allocatedSize))
 
 		// Table schemas are cached by tabletserver. For each table we cache `information_schema.tables.create_time` (`tbl.CreateTime`).
 		// We also record the last time the schema was loaded (`se.lastChange`). Both are in seconds. We reload a table only when:
@@ -372,8 +372,6 @@ func (se *Engine) reload(ctx context.Context) error {
 		//      #1 will not identify the renamed table as a changed one.
 		tbl, isInTablesMap := se.tables[tableName]
 		if isInTablesMap && createTime == tbl.CreateTime && createTime < se.lastChange {
-			tbl.FileSize = fileSize
-			tbl.AllocatedSize = allocatedSize
 			continue
 		}
 
@@ -383,8 +381,6 @@ func (se *Engine) reload(ctx context.Context) error {
 			rec.RecordError(err)
 			continue
 		}
-		table.FileSize = fileSize
-		table.AllocatedSize = allocatedSize
 		table.CreateTime = createTime
 		changedTables[tableName] = table
 		if isInTablesMap {
@@ -424,6 +420,72 @@ func (se *Engine) reload(ctx context.Context) error {
 		log.Infof("schema engine created %v, altered %v, dropped %v", created, altered, dropped)
 	}
 	se.broadcast(created, altered, dropped)
+	return nil
+}
+
+// reloadStats reloads schema statistics. It can also be used to initialize it.
+func (se *Engine) reloadStats(ctx context.Context) error {
+	defer func() {
+		se.env.LogError()
+	}()
+
+	conn, err := se.conns.Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer conn.Recycle()
+
+	// curTime will be saved into lastChange after schema is loaded.
+	curTime, err := se.mysqlTime(ctx, conn)
+	if err != nil {
+		return err
+	}
+	// if this flag is set, then we don't need table meta information
+	if se.SkipMetaCheck {
+		return nil
+	}
+	tableData, err := conn.Exec(ctx, conn.BaseShowTables(), maxTableCount, false)
+	if err != nil {
+		return err
+	}
+
+	err = se.updateInnoDBRowsRead(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	rec := concurrency.AllErrorRecorder{}
+	for _, row := range tableData.Rows {
+		tableName := row[0].ToString()
+		createTime, _ := evalengine.ToInt64(row[2])
+		fileSize, _ := evalengine.ToUint64(row[4])
+		allocatedSize, _ := evalengine.ToUint64(row[5])
+
+		// publish the size metrics
+		se.tableFileSizeGauge.Set(tableName, int64(fileSize))
+		se.tableAllocatedSizeGauge.Set(tableName, int64(allocatedSize))
+
+		// Table schemas are cached by tabletserver. For each table we cache `information_schema.tables.create_time` (`tbl.CreateTime`).
+		// We also record the last time the schema was loaded (`se.lastChange`). Both are in seconds. We reload a table only when:
+		//   1. A table's underlying mysql metadata has changed: `se.lastChange >= createTime`. This can happen if a table was directly altered.
+		//      Note that we also reload if `se.lastChange == createTime` since it is possible, especially in unit tests,
+		//      that a table might be changed multiple times within the same second.
+		//
+		//   2. A table was swapped in by Online DDL: `createTime != tbl.CreateTime`. When an Online DDL migration is completed the temporary table is
+		//      renamed to the table being altered. `se.lastChange` is updated every time the schema is reloaded (default: 30m).
+		//      Online DDL can take hours. So it is possible that the `create_time` of the temporary table is before se.lastChange. Hence,
+		//      #1 will not identify the renamed table as a changed one.
+		tbl, isInTablesMap := se.tables[tableName]
+		if isInTablesMap && createTime == tbl.CreateTime && createTime < se.lastChange {
+			tbl.FileSize = fileSize
+			tbl.AllocatedSize = allocatedSize
+			continue
+		}
+	}
+	if rec.HasErrors() {
+		return rec.Error()
+	}
+
 	return nil
 }
 
